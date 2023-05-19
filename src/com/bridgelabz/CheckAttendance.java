4297d5a03d2411a786b3540d22e54d1ad428134a
Your Name <nanawarechaitali4196@gmail.com>
2023-05-19 15:45:19 +0530

    Added UC4

--- src/com/bridgelabz/CheckAttendance.java
+++ src/com/bridgelabz/CheckAttendance.java
@@ -4,18 +4,30 @@ public class CheckAttendance {
     int Is_Present = 1;
     int Is_Absent = 0;
     int Full_Time = 1;
+    int Part_Time = 2;
+
     int Wage_Per_Hrs = 20;
-    int workingHrs = 8;
+    int workingHrs = 0;
 
     public void  CheckEmpAttendance(){
-        int checkAttendance =(int)(Math.random()*100)%2;
-        if(checkAttendance==Full_Time){
-            System.out.println("Employee is present");
+        int checkAttendance =(int)(Math.random()*100)%3;
+        switch(checkAttendance){
+            case 1 :
+                //if(checkAttendance==Full_Time){
+               System.out.println("Employee is present");
+               workingHrs=8;
+               break;
 
+            case 2 :
+                //else if(checkAttendance==Part_Time)
+                System.out.println("Employee is Present");
+                workingHrs = 4;
+                break;
+            default:
 
-        }else{
-            System.out.println("Employee is absent");
+                System.out.println("Employee is absent");
         }
+
         int wage = workingHrs*Wage_Per_Hrs;
         System.out.println("Employee Daily Wage is" + wage);
 
