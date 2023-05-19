a5dddb5bccf673f00db47e441433a9f07de807a1
Your Name <nanawarechaitali4196@gmail.com>
2023-05-19 15:15:35 +0530

    Added UC2

--- src/com/bridgelabz/CheckAttendance.java
+++ src/com/bridgelabz/CheckAttendance.java
@@ -1,22 +1,33 @@
 package com.bridgelabz;
 
 public class CheckAttendance {
-    int Is_Present =1;
-    int Is_Absent=0;
+    int Is_Present = 1;
+    int Is_Absent = 0;
+    int Full_Time = 1;
+    int Wage_Per_Hrs = 20;
+    int workingHrs = 8;
+
     public void  CheckEmpAttendance(){
         int checkAttendance =(int)(Math.random()*100)%2;
-        if(checkAttendance==Is_Present){
+        if(checkAttendance==Full_Time){
             System.out.println("Employee is present");
+
 
         }else{
             System.out.println("Employee is absent");
         }
+        int wage = workingHrs*Wage_Per_Hrs;
+        System.out.println("Employee Daily Wage is" + wage);
+
 
     }
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation program on Master branch");
+
         CheckAttendance obj = new CheckAttendance();
         obj.CheckEmpAttendance();
+
 
 
 
